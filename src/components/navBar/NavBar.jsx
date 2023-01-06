adbf69ff4057097ce3f239627391c6a7ff2c3f32
agusgar409 <62307436+agusgar409@users.noreply.github.com>
2023-01-05 23:57:34 -0300

    dropdown workin, title redirect to list products

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import CartWidget from '../cartWidget/CartWidget'
 
 const NavBar = () => {
@@ -9,14 +10,20 @@ const NavBar = () => {
           <div className="collapse navbar-collapse justify-content-around" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item">
-								<a className="nav-link active" aria-current="page" href="#home">NetShop</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#products">Products</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#categories">Categories</a>
-							</li>
+                <Link to={"/"} className="nav-link active" aria-current="page" href="#home">NetShop</Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#products">Products</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href='#category' type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categories
+                </a>
+                <ul class="dropdown-menu">
+                  <li><Link to={"/category"} class="dropdown-item" href="#action">Action</Link></li>
+                  <li><Link to={"/category"} class="dropdown-item" href="#another">Sport</Link></li>
+                </ul>
+              </li>
 						</ul>
 						<div>
 							<CartWidget/>
