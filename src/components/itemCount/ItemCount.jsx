448195d28f88eef565cd8ecf9beea4c0c064c059
agusgar409 <62307436+agusgar409@users.noreply.github.com>
2023-01-23 23:47:49 -0300

    changes on counter component

--- src/components/itemCount/ItemCount.jsx
+++ src/components/itemCount/ItemCount.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import { useState } from "react";
 
-const ItemCount = () => {
+const ItemCount = ({stock,initial,AddToCart}) => {
 
 
     
-    const [valueCounter, setValueCounter] = useState(0);
+    const [valueCounter, setValueCounter] = useState(initial);
 
-    const sumValue = () =>{
-        setValueCounter(valueCounter + 1)
-      }
-    
-    const discountValue = () => {
-    if(valueCounter > 0){
-        setValueCounter(valueCounter - 1);
+    const onPlus = () =>{
+        if(valueCounter < stock) setValueCounter(valueCounter + 1)
     }
+    
+    const onDecrement = () => {
+        if(valueCounter > initial) setValueCounter(valueCounter - 1);
     }
 
     return (
-        <div className='row'>
-            <div className='col-lg-3 align-self-center'>
-                <button onClick={discountValue} type="button" class="btn btn-outline-primary">-</button>
+        <div className='pt-3 row justify-content-between'>
+            <div className='col-lg-6'>
+                <div className='row'>
+                    <div className='col-lg-4 align-self-center'>
+                        <button onClick={onDecrement} type="button" class="btn btn-outline-primary">-</button>
+                    </div>
+                    <div className='col-lg-2 align-self-center'>
+                        <p className='text-center'>{valueCounter}</p>
+                    </div>
+                    <div className='col-lg-4 align-self-center'>
+                        <button onClick={onPlus} type="button" class="btn btn-outline-primary">+</button>
+                    </div>
+                </div>
             </div>
-            <div className='col-lg-1 align-self-center'>
-                <p className='text-center'>{valueCounter}</p>
-            </div>
-            <div className='col-lg-4 align-self-center'>
-                <button onClick={sumValue} type="button" class="btn btn-outline-primary">+</button>
+            <div className='col-lg-6 align-self-center'>
+                <button onClick={() => AddToCart} type="button" class="btn btn-outline-primary">Add to cart</button>
             </div>
         </div>
     )
--- src/components/itemDetails/ItemDetails.jsx
+++ src/components/itemDetails/ItemDetails.jsx
@@ -3,6 +3,10 @@ import ItemCount from '../itemCount/ItemCount'
 
 const ItemDetails = ({item}) => {
 
+    const AddToCart = () => {
+        alert("alert show")
+    }
+
     return (
         <div className='container center'>
             <div className='row ' >
@@ -10,22 +14,18 @@ const ItemDetails = ({item}) => {
                     <img style={{width: "18rem" }} src={item.image} alt="imageItem" />
                 </div>
                 <div className='col-lg-6 col-sm-12'>
-                <div>
-                    <h1>{item.title}</h1>
-                </div>
-                <div className='pt-5 row justify-content-center'>
-                <div className='pt-3'>
-                <h2>${item.price}</h2>
-            </div>
-            <div className='pt-3 row justify-content-between'>
-                <div className='col-lg-6'>
-                    <ItemCount></ItemCount>
-                </div>
-                <div className='col-lg-6 align-self-center'>
-                <button type="button" class="btn btn-outline-primary">Add to cart</button>
-                </div>
-            </div>
-                </div>
+                    <div>
+                        <h1>{item.title}</h1>
+                    </div>
+                    <div className='pt-5 row justify-content-center'>
+                        <div className='pt-3'>
+                            <h2>${item.price}</h2>
+                        </div>
+                        <ItemCount 
+                            stock={20} 
+                            initial={1}
+                            AddToCart={AddToCart()}/>
+                    </div>
                 </div>
             </div>
             <div className='pt-5'>
